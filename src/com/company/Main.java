da4446288b9e6158b6bbf31443bd1957b45d7b99
Konstantin Merdzhanov <kmerdjanov@gmail.com>
2018-05-31 10:47:15 +0300

    channel! modified to ch!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello git!");
-	    System.out.println("Hello git external change!");
+	    System.out.println("Hello git external ch!");
     }
 }
